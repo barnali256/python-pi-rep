06bd6e2721e1ebeda11b730e99beb35426af43b1
barnali <barnalib05@gmail.com>
2020-04-02 12:34:02 +0530

    merged conflicts res

--- myfile.py
+++ myfile.py
@@ -4,11 +4,6 @@
 def msg(message):
     print(message)
 //changes made for second time
-
-<<<<<<< HEAD
-msg("hello people")
-=======
-
 msg("hello people")
 //print("new changes")
->>>>>>> refs/remotes/origin/master
+
